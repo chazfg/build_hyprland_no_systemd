55ca791489574047397c2a87adf4d1b09c30ab0d
chazfg <81254302+chazfg@users.noreply.github.com>
2024-11-12 08:40:28 -0600

    The script

---
+++ hypr_inst.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DNO_SYSTEMD:STRING=true -B build -G Ninja
+cmake --build ./build --config Release --target all
+sudo cmake --install ./build
